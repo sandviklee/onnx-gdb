009a5a920ac5c1bd554daba8fa3446c375de0f0c
Simon Sandvik Lee <simonsandviklee@gmail.com>
2026-02-23 21:23:49 +0100

    feat: Setup boir structure (for future reference)

---
+++ include/boir/boir.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef BOIDPARSER_H
+#define BOIDPARSER_H
+
+class BoirParser {
+public: // TODO: Define public methods
+  BoirParser();
+  void updateValue();
+
+private: // TODO: Define private methods
+};
+
+#endif
---
+++ src/boir/boir.cpp
@@ -0,0 +1,6 @@
+#include <boir/boir.h>
+#include <iostream>
+
+BoirParser::BoirParser() { std::cout << "Hello, Constructor!" << std::endl; };
+
+void BoirParser::updateValue() { std::cout << "Hello, Parser!" << std::endl; }
