e75a9683f0df7f1461a34d07de2cd229c60ab3ec
Simon Sandvik Lee <simonsandviklee@gmail.com>
2026-03-29 23:09:45 +0200

    feat: Add toolbar

--- include/ui/toolbar.h
+++ include/ui/toolbar.h
@@ -1,11 +1,41 @@
-#pragma once
 #include "raylib.h"
 #include <vector>
 
-struct toolbar_button {
+#pragma once
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+enum class ToolbarButtonType { POINTER, LIBRARY, DEBUG, INFERENCE };
+
+constexpr ToolbarButtonType all_types[] = {
+    ToolbarButtonType::POINTER, ToolbarButtonType::LIBRARY,
+    ToolbarButtonType::DEBUG, ToolbarButtonType::INFERENCE};
+
+class ToolbarButton {
+  friend class Toolbar;
+
+private:
   Rectangle rect;
   const char *text;
+  const ToolbarButtonType type;
+
+public:
+  ToolbarButton(const ToolbarButtonType type, const float size,
+                const Vector2 position);
+  void draw();
 };
 
-void draw_toolbar(const std::vector<toolbar_button> buttons);
-int handle_toolbar_click(const std::vector<toolbar_button> &buttons);
+class Toolbar {
+private:
+  std::vector<ToolbarButton> buttons;
+  float height;
+  float width;
+  Vector2 position;
+
+public:
+  Toolbar(const Vector2 position, const float button_size);
+  void draw();
+  void handle_click();
+};
+
+#endif
--- src/ui/toolbar.cpp
+++ src/ui/toolbar.cpp
@@ -1,19 +1,61 @@
 #include "ui/toolbar.h"
+#include "raylib.h"
+#include "ui/block.h"
+#include <iostream>
 
-void draw_toolbar(const std::vector<toolbar_button> buttons) {
-  for (const auto &button : buttons) {
-    DrawRectangleRec(button.rect, GRAY);
-    DrawText(button.text, button.rect.x + 10, button.rect.y + 10, 20, BLACK);
+ToolbarButton::ToolbarButton(const ToolbarButtonType type, const float size,
+                             const Vector2 position)
+    : type(type) {
+  switch (type) {
+  case ToolbarButtonType::POINTER:
+    text = "P";
+    break;
+  case ToolbarButtonType::LIBRARY:
+    text = "L";
+    break;
+  case ToolbarButtonType::DEBUG:
+    text = "D";
+    break;
+  case ToolbarButtonType::INFERENCE:
+    text = "I";
+    break;
+  }
+  rect = {position.x, position.y, size, size};
+}
+
+Toolbar::Toolbar(const Vector2 position, const float button_size)
+    : position(position) {
+  size_t size_all_types = std::size(all_types);
+
+  width = size_all_types * (button_size + TOOLBAR_PADDING) + TOOLBAR_PADDING;
+  height = button_size + TOOLBAR_PADDING * 2;
+
+  for (size_t i = 0; i < size_all_types; i++) {
+    ToolbarButtonType type = all_types[i];
+    Vector2 button_position = {
+        position.x + i * (button_size + TOOLBAR_PADDING) + TOOLBAR_PADDING,
+        position.y + TOOLBAR_PADDING};
+    buttons.push_back(ToolbarButton(type, button_size, button_position));
   }
 }
 
-int handle_toolbar_click(const std::vector<toolbar_button> &buttons) {
-  if (!IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-    return -1;
-  Vector2 mouse = GetMousePosition();
-  for (size_t i = 0; i < buttons.size(); i++) {
-    if (CheckCollisionPointRec(mouse, buttons[i].rect))
-      return i;
+void Toolbar::draw() {
+  DrawRectangleRec({position.x, position.y, width, height}, LIGHTGRAY);
+  for (const auto &button : buttons) {
+    DrawRectangleRec(button.rect, GRAY);
+    DrawText(button.text, (button.rect.x - 5) + button.rect.width / 2,
+             (button.rect.y - 10) + button.rect.height / 2, 20, BLACK);
   }
-  return -1;
+}
+
+void Toolbar::handle_click() {
+  if (!IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    return;
+
+  Vector2 mouse = GetMousePosition();
+  for (ToolbarButton &button : buttons) {
+    if (CheckCollisionPointRec(mouse, button.rect)) {
+      std::cout << "Clicked on button: " << button.text << std::endl;
+    }
+  }
 }
