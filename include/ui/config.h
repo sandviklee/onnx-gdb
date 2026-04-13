8b614ace8b017ca91bb318900deaad48b95fa87c
Simon Sandvik Lee <simonsandviklee@gmail.com>
2026-04-13 21:01:18 +0200

    chore: Add LIBRARY configs

--- include/ui/config.h
+++ include/ui/config.h
@@ -13,8 +13,10 @@ static constexpr float FIELD_START_H = 38.0f;
 static constexpr float PORT_RADIUS = 8.0f;
 static constexpr float WIRE_THICK = 3.0f;
 
-static const Color COLOR_PORT_INPUT = {100, 180, 255, 255};
-static const Color COLOR_RELU = {255, 180, 80, 255};
+static constexpr float LIBRARY_ITEM_SIZE = 100.0f;
+static constexpr float LIBRARY_H = 300.0f;
+static constexpr float LIBRARY_W = 260.0f;
+
 static const Color COLOR_PORT_OUTPUT = {100, 220, 130, 255};
 static const Color COLOR_FIELD_BG = {240, 240, 240, 255};
 static const Color COLOR_FIELD_ACTIVE = {255, 255, 220, 255};
